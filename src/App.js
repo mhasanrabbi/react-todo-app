61d87791d092047c310e4ba7c91414fe13dc60b9
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-03-21 21:05:44 +0600

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+// Importing Components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
