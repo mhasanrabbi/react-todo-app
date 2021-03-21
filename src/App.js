9fd17dfd4973b3990c4754c3459f9a2db78d5d7c
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-03-21 21:45:43 +0600

    include handler

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <header>
         <h1>Todo List </h1>
       </header>
-      <Form setInputText={setInputText}/>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
       <TodoList />
     </div>
   );
